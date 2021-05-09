ceb2715477122307d254605a67b7abc1bee3d8b7
Lntechnical <82169706+lntechnical2@users.noreply.github.com>
2021-05-09 15:49:27 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,6 +11,7 @@ from pyrogram.types import (
 )
 from pyrogram.types import CallbackQuery
 from google_trans_new import google_translator
+from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 
 
 import pyrogram
@@ -38,7 +39,15 @@ def start(client, message):
 	
 @app.on_message(filters.text & filters.private )
 def echo(client, message):
- 
+ update_channel = "lntechnical"
+ user_id = message.from_user.id
+ if update_channel :
+  try:
+   client.get_chat_member(update_channel, user_id)
+  except UserNotParticipant:
+   message.reply_text("**__You are not subscribed my channel__** ",parse_mode="markdown", reply_to_message_id = message.message_id, reply_markup = InlineKeyboardMarkup([ [ InlineKeyboardButton("Support 🇮🇳" ,url="https://t.me/lntechnical") ]
+   ]))
+   return
  keybord = InlineKeyboardMarkup( [
         [
             InlineKeyboardButton("Hindi", callback_data='hi'),
