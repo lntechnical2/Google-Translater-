34df69ed1458d71c690c0e452a55983137c73fb5
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-06-12 10:08:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,7 +29,34 @@ app = Client(
         bot_token=TOKEN,api_hash=API_HASH,
             api_id=APP_ID
     )
-    
+
+keyboard = InlineKeyboardMarkup(
+    [[
+    InlineKeyboardButton("മലയാളം", callback_data="Malayalam"),
+    InlineKeyboardButton("தமிழ்", callback_data="Tamil"),
+    InlineKeyboardButton("हिन्दी", callback_data="Hindi")
+    ],[
+    InlineKeyboardButton("ಕನ್ನಡ", callback_data="Kannada"),
+    InlineKeyboardButton("తెలుగు", callback_data="Telugu"),
+    InlineKeyboardButton("मराठी", callback_data="Marathi")
+    ],[
+    InlineKeyboardButton("ગુજરાતી", callback_data="Gujarati"),
+    InlineKeyboardButton("ଓଡ଼ିଆ", callback_data="Odia"),
+    InlineKeyboardButton("বাংলা", callback_data="bn")
+    ],[
+    InlineKeyboardButton("ਪੰਜਾਬੀ", callback_data="Punjabi"),
+    InlineKeyboardButton("فارسی", callback_data="Persian"),
+    InlineKeyboardButton("English", callback_data="English")
+    ],[
+    InlineKeyboardButton("español", callback_data="Spanish"),
+    InlineKeyboardButton("français", callback_data="French"),
+    InlineKeyboardButton("русский", callback_data="Russian")
+    ],[
+    InlineKeyboardButton("עִברִית", callback_data="hebrew"),
+    InlineKeyboardButton("العربية", callback_data="arabic"),
+    ]]
+)
+
 @app.on_message(filters.command(['start']))
 def start(client, message):
             message.reply_text(text =f"Hello **{message.from_user.first_name }** \n\n __I am simple Google Translater Bot \n I can translate any language to you selected language__",reply_to_message_id = message.message_id , parse_mode="markdown", reply_markup=InlineKeyboardMarkup(
@@ -40,38 +67,20 @@ def start(client, message):
            ]
         ) )
 	
-@app.on_message(filters.text & filters.private )
-def echo(client, message):
- 
- keybord = InlineKeyboardMarkup( [
-        [
-            InlineKeyboardButton("Hindi", callback_data='hi'),
-            InlineKeyboardButton("Kannada", callback_data='kn'),
-            InlineKeyboardButton("malayalam",callback_data ='ml')
-        ],
-        [   InlineKeyboardButton("Tamil", callback_data='ta'),
-        InlineKeyboardButton("Telugu", callback_data='te'),
-        InlineKeyboardButton("English",callback_data = 'en')
-        ],
-        	[InlineKeyboardButton("Urdu",callback_data ="ur"),
-	InlineKeyboardButton("Punjabi",callback_data="pa"),
-	InlineKeyboardButton("Spanish",callback_data="es")
-	]
-    ]
- 
- )
+@app.on_message(filters.text & filters.private)
+async def echo(client, message):
+    await message.reply_text(
+        text = "Select language 👇",
+        reply_markup = keybord
+    )
 
- 
- message.reply_text("Select language 👇",reply_to_message_id = message.message_id, reply_markup = keybord)
-    
-    
 @app.on_callback_query()
-async def translate_text(bot,update):
-  tr_text = update.message.reply_to_message.text
-  cbdata = update.data
-  translator = google_translator()
-  translated_text = translator.translate(tr_text,lang_tgt=cbdata)
-  await update.message.edit(translated_text)
+async def translate_text(bot, update):
+    tr_text = update.message.reply_to_message.text
+    cbdata = update.data
+    translator = google_translator()
+    translated_text = translator.translate(tr_text, lang_tgt=cbdata)
+    await update.message.edit(translated_text)
   	
 
 app.run()
