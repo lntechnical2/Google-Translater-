d551db99e00326eee17085e023ed95c7a88f2cd4
Lntechnical <82169706+lntechnical2@users.noreply.github.com>
2021-07-05 15:06:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,13 +9,13 @@ from pyrogram.types import (
 from googletrans import Translator
 TOKEN = os.environ.get("TOKEN", "")
 
-APP_ID = int(os.environ.get("API_ID", 12345))
+API_ID = int(os.environ.get("API_ID", 12345))
 
 API_HASH = os.environ.get("API_HASH", "")
 app = Client(
         "ggt",
         bot_token=TOKEN,api_hash=API_HASH,
-            api_id=APP_ID
+            api_id=API_ID
     )
 
 
