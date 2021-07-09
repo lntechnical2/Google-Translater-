e4ffa4bbd9e360ad0937b2ecba3962f4609e4ea5
Lntechnical <82169706+lntechnical2@users.noreply.github.com>
2021-07-09 13:51:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,9 +13,10 @@ API_ID = int(os.environ.get("API_ID", 12345))
 
 API_HASH = os.environ.get("API_HASH", "")
 app = Client(
-        "ggt",TOKEN,API_ID,API_HASH
+        "Gtt",
+        bot_token=TOKEN,api_hash=API_HASH,
+            api_id=API_ID
     )
-
 
 @app.on_message(filters.private & filters.command(['start']))
 async def start(client, message):
